cc3b50cfff081da0f256c61723448ba65f13e147
Yuvraj Srivastav <sriva@Lappy.com>
2025-09-24 20:02:49 +0530

    Commit

---
+++ Program15.py
@@ -0,0 +1,8 @@
+#Program to find the factorial of a number
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+num = 5
+print("The factorial of", num, "is", factorial(num))
