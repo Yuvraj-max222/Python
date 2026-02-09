65b7f01f7ecff27e167893a139ae7fe4587ecc10
Yuvraj Srivastav <sriva@Lappy.com>
2026-02-09 20:55:24 +0530

    PPPP

--- Program19.py
+++ Program19.py
@@ -1,13 +1,13 @@
 #Write a Python Program to Count the Occurrences of every words in a Text File.
-def word_count_in_file(file_path):
-    freq = {}
+def word_count(file_path):
+    counts = {}    
     with open(file_path, 'r') as file:
         for line in file:
             words = line.split()
             for word in words:
                 word = word.lower()
-                if word in freq:
-                    freq[word] += 1
+                if word in counts:
+                    counts[word] += 1
                 else:
-                    freq[word] = 1
-    return freq
+                    counts[word] = 1
+    return counts  
